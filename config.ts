e9103d9d145f4efe56806c1924b3a8ef72c68a77
Abhiram A R <abhiramajithr@gmail.com>
2026-07-30 21:56:12 +0530

    reset

--- config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const vars = {
-    DATABASE_URL: process.env.DATABASE_URL
-}
-
-export default vars;
--- src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import vars from '../../config';
-
-const db = drizzle(vars.DATABASE_URL!);
-
-export default db;
